e23d3f8fa3e9de4e9ea82514180d4870be25277b
Daniel Wirtz <daniel.wirtz@mathematik.uni-stuttgart.de>
2011-08-01 10:50:44 +0200

    Initial commit.

---
+++ KerMORDSApp/src/kermor/app/KerMORDSAppActivity.java
@@ -0,0 +1,13 @@
+package kermor.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KerMORDSAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
